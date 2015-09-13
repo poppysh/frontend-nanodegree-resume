c99adef0d36a284f72d35441c0bcd08a5f449b5e
szeenberg <sarahzeenberg@gmail.com>
2015-09-13 15:57:38 -0400

    Add header info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//header
+var name = "Sarah Zeenberg";
+var formattedName = HTMLheaderName.replace(
+	"%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace(
+	"%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
